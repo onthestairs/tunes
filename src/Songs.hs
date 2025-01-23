0528107482e4c6805e0ffd7bd652de86c27e7da5
Austin Platt <austinplatt@gmail.com>
2025-01-23 23:50:37 +0000

    add bmp calculation

--- src/Songs.hs
+++ src/Songs.hs
@@ -1,6 +1,12 @@
 module Songs where
 
+import Relude
 import Songs.Roses (roses)
 import Sound.PlSynth (withPlSynth, withSongTracks, writeSong)
 
-main = withPlSynth $ withSongTracks 8481 roses $ writeSong "test-song.wav"
+bpm :: Word32 -> Word32
+bpm n = 60 * 44100 `div` 4 `div` n
+
+main :: IO ()
+main = do
+  withPlSynth $ withSongTracks (bpm 70) roses $ writeSong "test-song.wav"
