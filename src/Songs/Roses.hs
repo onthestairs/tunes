0eb4438c3b2c2a32da2d4ad3182b1b5bdcdc8f63
Austin Platt <austinplatt@gmail.com>
2025-01-24 22:53:23 +0000

    use dsl for roses, and tidy

--- src/Songs/Roses.hs
+++ src/Songs/Roses.hs
@@ -1,37 +1,11 @@
 module Songs.Roses (roses) where
 
-import Relude (Word8)
-import Sound.PlSynth (PlSynthT (..), emptyPlSynthT)
+import Dsl
+import Relude
+import Sound.PlSynth (PlSynthT (..))
 
-synth :: PlSynthT
-synth =
-  emptyPlSynthT
-    { osc0_oct = 7,
-      osc0_vol = 121,
-      osc0_waveform = 1,
-      osc1_oct = 7,
-      osc1_vol = 91,
-      osc1_waveform = 3,
-      env_attack = 100,
-      env_sustain = 1212,
-      env_release = 5513,
-      env_master = 100,
-      fx_freq = 6,
-      fx_resonance = 19,
-      fx_delay_time = 3,
-      fx_delay_amt = 121,
-      fx_pan_freq = 6,
-      fx_pan_amt = 21,
-      lfo_fx_freq = 1,
-      lfo_freq = 1,
-      lfo_amt = 29
-    }
-
-synth1 :: PlSynthT
-synth1 = PlSynthT 7 0 0 0 121 1 7 0 0 0 91 3 0 100 1212 5513 113 0 6 19 3 121 6 21 0 1 1 29 0
-
-synth2 :: PlSynthT
-synth2 = PlSynthT 9 0 0 0 255 0 9 0 12 0 255 0 0 100 0 14545 70 0 0 240 2 157 3 47 0 0 0 0 0
+leadSynth :: PlSynthT
+leadSynth = PlSynthT 9 0 0 0 255 0 9 0 12 0 255 0 0 100 0 14545 70 0 0 240 2 157 3 47 0 0 0 0 0
 
 bassDrum :: PlSynthT
 bassDrum = PlSynthT 7 0 0 1 255 0 7 0 0 1 255 0 0 100 0 3636 254 2 500 254 0 27 0 0 0 0 0 0 0
@@ -50,15 +24,17 @@ fourOnTheFloor :: [Word8]
 fourOnTheFloor = replicateL 16 [147, 0]
 
 roses :: [([Word8], [[Word8]], PlSynthT)]
-roses = [t1, t2]
-  where
-    t1 =
-      ( [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
-        [riff1, riff2],
-        synth2
-      )
-    t2 =
-      ( [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [fourOnTheFloor],
-        bassDrum
-      )
+roses = build $ do
+  lead <- synth leadSynth
+  bass <- synth bassDrum
+  leadPattern1 <- patt lead riff1
+  leadPattern2 <- patt lead riff2
+  four <- patt bass fourOnTheFloor
+  pure
+    [ [four, leadPattern1],
+      [four, leadPattern2],
+      [four, leadPattern1],
+      [four, leadPattern2],
+      [four, leadPattern1],
+      [four, leadPattern2]
+    ]
