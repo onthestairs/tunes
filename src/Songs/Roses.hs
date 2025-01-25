f8eb65cb03c02458842dd1bd2c624636e2f6f198
Austin Platt <austinplatt@gmail.com>
2025-01-25 21:51:16 +0000

    tidy

--- src/Songs/Roses.hs
+++ src/Songs/Roses.hs
@@ -9,40 +9,32 @@ leadSynth :: PlSynthT
 leadSynth = PlSynthT 9 0 0 0 255 0 9 0 12 0 255 0 0 100 0 14545 70 0 0 240 2 157 3 47 0 0 0 0 0
 
 makeRiff :: [Note] -> [Note]
-makeRiff bassNotes = concatMap (\n -> bassNotes <> [n]) [66, 65, 61, 70, 65] <> [66]
+makeRiff bassNotes = concatMap (\n -> bassNotes <> [n]) [141, 140, 136, 145, 140] <> [141]
 
 riff1, riff2 :: [Note]
-riff1 = map (+ 75) (makeRiff [51, 58] <> makeRiff [51, 58])
-riff2 = map (+ 75) (makeRiff [47, 54] <> makeRiff [49, 56])
+riff1 = makeRiff [126, 133] <> makeRiff [126, 133]
+riff2 = makeRiff [122, 129] <> makeRiff [124, 131]
 
 replicateL :: Int -> [a] -> [a]
 replicateL n = concat . replicate n
 
-fourOnTheFloor :: [Note]
-fourOnTheFloor = replicateL 16 [147, 0]
-
-syncopatedFour :: [Note]
-syncopatedFour = replicateL 16 [0, 147]
-
 roses :: Song
 roses = build $ do
   bass <- synth Synths.bassDrum
-  bass0 <- patt bass fourOnTheFloor
+  fourOnTheFloor <- patt bass (replicateL 16 [147, 0])
 
   hihat <- synth Synths.hihat
-  hihat0 <- patt hihat syncopatedFour
+  hihatSync <- patt hihat (replicateL 16 [0, 147])
 
   lead <- synth leadSynth
   lead0 <- patt lead riff1
   lead1 <- patt lead riff2
 
   pure
-    [ [bass0],
-      [bass0, hihat0],
-      [bass0, hihat0, lead0],
-      [bass0, hihat0, lead1],
-      [bass0, hihat0, lead0],
-      [bass0, hihat0, lead1],
-      [bass0, hihat0, lead0],
-      [bass0, hihat0, lead1]
+    [ [fourOnTheFloor],
+      [fourOnTheFloor, hihatSync],
+      [fourOnTheFloor, hihatSync, lead0],
+      [fourOnTheFloor, hihatSync, lead1],
+      [fourOnTheFloor, hihatSync, lead0],
+      [fourOnTheFloor, hihatSync, lead1]
     ]
