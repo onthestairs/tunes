b41a9e5681366f84834bb7cca3f9cc16e9a7d3e4
Austin Platt <austinplatt@gmail.com>
2025-01-25 23:57:24 +0000

    use note names rather than numbers

--- src/Songs/Roses.hs
+++ src/Songs/Roses.hs
@@ -1,6 +1,7 @@
 module Songs.Roses (roses) where
 
 import Dsl
+import Notes
 import Relude
 import Sound.PlSynth (PlSynthT (..))
 import Synths qualified
@@ -9,11 +10,11 @@ leadSynth :: PlSynthT
 leadSynth = PlSynthT 9 0 0 0 255 0 9 0 12 0 255 0 0 100 0 14545 70 0 0 240 2 157 3 47 0 0 0 0 0
 
 makeRiff :: [Note] -> [Note]
-makeRiff bassNotes = concatMap (\n -> bassNotes <> [n]) [141, 140, 136, 145, 140] <> [141]
+makeRiff bassNotes = concatMap (\n -> bassNotes <> [n]) [fs4, f4, cs4, as4, f4] <> [fs4]
 
 riff1, riff2 :: [Note]
-riff1 = makeRiff [126, 133] <> makeRiff [126, 133]
-riff2 = makeRiff [122, 129] <> makeRiff [124, 131]
+riff1 = makeRiff [ds3, as3] <> makeRiff [ds3, as3]
+riff2 = makeRiff [b2, fs3] <> makeRiff [cs3, gs3]
 
 replicateL :: Int -> [a] -> [a]
 replicateL n = concat . replicate n
@@ -21,10 +22,10 @@ replicateL n = concat . replicate n
 roses :: Song
 roses = build $ do
   bass <- synth Synths.bassDrum
-  fourOnTheFloor <- patt bass (replicateL 16 [147, 0])
+  fourOnTheFloor <- patt bass (replicateL 16 [c5, 0])
 
   hihat <- synth Synths.hihat
-  hihatSync <- patt hihat (replicateL 16 [0, 147])
+  hihatSync <- patt hihat (replicateL 16 [0, c5])
 
   lead <- synth leadSynth
   lead0 <- patt lead riff1
